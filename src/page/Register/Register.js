a532180977913c9377f577abfb2d3c3253306ab0
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-20 23:19:49 +0700

    #

--- src/page/Register/Register.js
+++ src/page/Register/Register.js
@@ -1,16 +1,16 @@
-import { InputGroup } from "react-bootstrap-v5";
 
-export default function Register ()
-{
+export default function Register (){
     return(
         <div className="page">
             <div className="title">Create an account </div>
             <div className="form">
                   <h2>Sign up with your email</h2>
-                <div>
-                    <div>Username</div>
-                    
-                </div>
+                <form className="">
+                    <label htmlFor="email">Username</label>
+                    <input id="email"  type="email"/>
+                    <label htmlFor="password">Password</label>
+                    <input id="password"/>
+                </form>
             </div>
         </div>
     )
