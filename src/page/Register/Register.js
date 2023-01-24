61acd3b19c1564a41501955e4ebd9840eaf78a74
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-24 23:29:50 +0700

    register UI

--- src/page/Register/Register.js
+++ src/page/Register/Register.js
@@ -4,24 +4,33 @@ export default function Register (){
     return(
         <div className="page">
             <Header></Header>
-            <div className="form">
-            <div className="title">Create an account </div>
-                  <h2>Sign up with your email</h2>
-                <form className="">
-                    <label htmlFor="email">Username</label>
-                    <input id="email"  type="email"/>
-                    <label htmlFor="password">Password</label>
-                    <input id="password"/>
+            <div className="section">
+            <div className="title">
+               <h2> Create an account</h2>
+               <h3>Sign up with your email</h3>
+            </div>
+            <div>          
+                <form className="form">
+                    <label htmlFor="email" className="formLabel">Email</label>
+                    <input id="email" className="formInput" type="email"/>
+                    <label htmlFor="password" className="formLabel">Password</label>
+                    <input id="password" className="formInput"/>
                     <button className="showPass">
                     </button>
-                    <button  type="submit" className="text--b btn__submit">Sign up</button>
-                </form>
-                <div className="recievedInfo">
+                    <button  type="submit" className="SubmitBtn">Sign up</button>
+                    <div className="recievedInfo">
                     <input className="recieveInfo_cb" type="checkbox"></input>
                     <span>I wish to receive information, offers, recommendations, and updates from Kahoot!</span>
                 </div>
-                <span>Already have an account? <a href="/login">Login</a></span>
+                </form>
+                <div className="form__seperator">
+                    <hr className="form__cardLine"></hr>
+                    <span className="form__Or">Or</span>
+                </div>
+                <span className="form__redirect">Already have an account? <a href="/login">Login</a></span>
+            </div>
             </div>
         </div>
+
     )
 }
