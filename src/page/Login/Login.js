6907b09df9f43dde725a0ae8550d188c8c8c1cbf
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-26 23:38:06 +0700

    handle show Pass by useState (Register & Login)

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,6 +1,30 @@
 import Header from "../../components/Header/Header"
 import './Login.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
+import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
 export default function Login(){
+    const [showPass, setShowPass] = useState(faEyeSlash);
+    const [showPassState, setShowPassState] = useState(false);
+    const [passType, setPassType] = useState('password')
+  
+    const handleShowPass = (e) => {
+        e.preventDefault();
+        if(showPassState)
+        {
+            //unshow
+            setPassType('password');
+            setShowPass(faEyeSlash);
+            setShowPassState(false);
+        }
+        else
+        {
+            //show
+            setPassType('text');
+            setShowPass(faEye);
+            setShowPassState(true);
+        }
+    }
     return(
         <div className="page">
             <Header></Header>
@@ -13,8 +37,12 @@ export default function Login(){
                     <label htmlFor="email" className="formLabel">Username or Email</label>
                     <input id="email" className="formInput" type="email"/>
                     <label htmlFor="password" className="formLabel">Password</label>
-                    <input id="password" className="formInput"/>
-                    <button className="showPass"> </button>
+                    <div className="passWordWrapper ">
+                    <input id="password" className="formInput" type={passType}/>
+                    <button className="showPassBtn" onClick={handleShowPass}>
+                        <FontAwesomeIcon className="eyeIcon" icon={showPass} size="lg"/>
+                    </button>
+                    </div>
                     <span className="form_text">Forgot password? <a href="/forgotPass"> Reset your password?</a> </span>
                     <button  type="submit" className="SubmitBtn">Login</button>
                 </form>
--- src/page/Register/Register.js
+++ src/page/Register/Register.js
@@ -1,6 +1,32 @@
 import Header from "../../components/Header/Header"
 import './Register.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
+import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
+
 export default function Register (){
+
+    const [showPass, setShowPass] = useState(faEyeSlash);
+    const [showPassState, setShowPassState] = useState(false);
+    const [passType, setPassType] = useState('password')
+  
+    const handleShowPass = (e) => {
+        e.preventDefault();
+        if(showPassState)
+        {
+            //unshow
+            setPassType('password');
+            setShowPass(faEyeSlash);
+            setShowPassState(false);
+        }
+        else
+        {
+            //show
+            setPassType('text');
+            setShowPass(faEye);
+            setShowPassState(true);
+        }
+    }
     return(
         <div className="page">
             <Header></Header>
@@ -14,9 +40,12 @@ export default function Register (){
                     <label htmlFor="email" className="formLabel">Email</label>
                     <input id="email" className="formInput" type="email"/>
                     <label htmlFor="password" className="formLabel">Password</label>
-                    <input id="password" className="formInput"/>
-                    <button className="showPass">
+                    <div className="passWordWrapper ">
+                    <input id="password" className="formInput" type={passType}/>
+                    <button className="showPassBtn" onClick={handleShowPass}>
+                        <FontAwesomeIcon className="eyeIcon" icon={showPass} size="lg"/>
                     </button>
+                    </div>
                     <button  type="submit" className="SubmitBtn">Sign up</button>
                     <div className="recievedInfo">
                     <input className="recieveInfo_cb" type="checkbox"></input>
