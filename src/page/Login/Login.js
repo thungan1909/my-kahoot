1d88a0a1f37d849a97a1d628d2259cf97ecf4219
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-27 14:34:34 +0700

    validation form by yub & formik

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -2,8 +2,13 @@ import Header from "../../components/Header/Header"
 import './Login.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
+import {useFormik} from "formik"
+import * as Yup from "yup"
+import { useMutation } from "react-query"
+import axios from 'axios'
 import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
 export default function Login(){
+    const [user, setUser] = useState();
     const [showPass, setShowPass] = useState(faEyeSlash);
     const [showPassState, setShowPassState] = useState(false);
     const [passType, setPassType] = useState('password')
@@ -25,6 +30,42 @@ export default function Login(){
             setShowPassState(true);
         }
     }
+    const formik = useFormik ({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required("Required")
+            .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,"Please enter a valid email address"),
+            password: Yup.string()
+             .required("Required")
+             .matches(
+               /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{7,19}$/,
+               "Password must be 7-19 characters and contain at least one letter, one number and a special character"
+             ),
+        }),
+        onSubmit: values => {
+          
+            setUser(values);
+           // mutate();
+        }
+    });
+    const {isLoading, isError, isSuccess, error, mutate} = useMutation(
+        postData,
+        {
+            onSuccess: (res) => {
+
+            },
+            onError: (err) => {
+
+            },
+        }
+    );
+    async function postData () {
+        return await axios.post("http://54.179.150.210:8000/auth/register", user)
+    }
+
     return(
         <div className="page">
             <Header></Header>
@@ -33,16 +74,17 @@ export default function Login(){
                <h2>Login</h2>
             </div>
             <div>          
-                <form className="form">
+                 <form className="form" onSubmit={formik.handleSubmit} >
                     <label htmlFor="email" className="formLabel">Username or Email</label>
-                    <input id="email" className="formInput" type="email"/>
+                    <input id="email" className="formInput" type="email" value={formik.values.email} onChange={formik.handleChange}/>
+                    {formik.errors.email && (<p className="errMsg">{formik.errors.email}</p>)}
                     <label htmlFor="password" className="formLabel">Password</label>
                     <div className="passWordWrapper ">
-                    <input id="password" className="formInput" type={passType}/>
-                    <button className="showPassBtn" onClick={handleShowPass}>
+                    <input id="password" className="formInput" type={passType} value={formik.values.password} onChange={formik.handleChange}/>                    <button className="showPassBtn" onClick={handleShowPass}>
                         <FontAwesomeIcon className="eyeIcon" icon={showPass} size="lg"/>
                     </button>
                     </div>
+                    {formik.errors.password && (<p className="errMsg">{formik.errors.password}</p>)}
                     <span className="form_text">Forgot password? <a href="/forgotPass"> Reset your password?</a> </span>
                     <button  type="submit" className="SubmitBtn">Login</button>
                 </form>
--- src/page/Register/Register.js
+++ src/page/Register/Register.js
@@ -1,11 +1,16 @@
 import Header from "../../components/Header/Header"
 import './Register.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react";
 import { useEffect, useState } from "react"
+import { Formik, useFormik} from "formik"
+import * as Yup from "yup"
 import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
-
+import { useMutation } from "react-query"
+import axios from 'axios'
 export default function Register (){
 
+    const [user, setUser] = useState();
     const [showPass, setShowPass] = useState(faEyeSlash);
     const [showPassState, setShowPassState] = useState(false);
     const [passType, setPassType] = useState('password')
@@ -27,6 +32,42 @@ export default function Register (){
             setShowPassState(true);
         }
     }
+    const formik = useFormik ({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required("Required")
+            .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,"Please enter a valid email address"),
+            password: Yup.string()
+             .required("Required")
+             .matches(
+               /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{7,19}$/,
+               "Password must be 7-19 characters and contain at least one letter, one number and a special character"
+             ),
+        }),
+        onSubmit: values => {
+          
+            setUser(values);
+           // mutate();
+        }
+    });
+    const {isLoading, isError, isSuccess, error, mutate} = useMutation(
+        postData,
+        {
+            onSuccess: (res) => {
+
+            },
+            onError: (err) => {
+
+            },
+        }
+    );
+    async function postData () {
+        return await axios.post("http://54.179.150.210:8000/auth/register", user)
+    }
+
     return(
         <div className="page">
             <Header></Header>
@@ -36,21 +77,23 @@ export default function Register (){
                <h3>Sign up with your email</h3>
             </div>
             <div>          
-                <form className="form">
+                <form className="form" onSubmit={formik.handleSubmit} >
                     <label htmlFor="email" className="formLabel">Email</label>
-                    <input id="email" className="formInput" type="email"/>
+                    <input id="email" className="formInput" type="email" value={formik.values.email} onChange={formik.handleChange}/>
+                    {formik.errors.email && (<p className="errMsg">{formik.errors.email}</p>)}
                     <label htmlFor="password" className="formLabel">Password</label>
                     <div className="passWordWrapper ">
-                    <input id="password" className="formInput" type={passType}/>
+                    <input id="password" className="formInput" type={passType} value={formik.values.password} onChange={formik.handleChange}/>
                     <button className="showPassBtn" onClick={handleShowPass}>
                         <FontAwesomeIcon className="eyeIcon" icon={showPass} size="lg"/>
                     </button>
                     </div>
-                    <button  type="submit" className="SubmitBtn">Sign up</button>
+                    {formik.errors.password && (<p className="errMsg">{formik.errors.password}</p>)}
+                    <button  type="submit" className="SubmitBtn"  >Sign up</button>
                     <div className="recievedInfo">
                     <input className="recieveInfo_cb" type="checkbox"></input>
                     <span>I wish to receive information, offers, recommendations, and updates from Kahoot!</span>
-                </div>
+                    </div>
                 </form>
                 <div className="form__seperator">
                     <hr className="form__cardLine"></hr>
