031c236d21badebfc7e57c0e02fc91615fac5d29
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-28 22:17:38 +0700

    verify email UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./page/Login/Login";
 import Register from "./page/Register/Register";
+import VerifyEmail from "./page/VerifyEmail/VerifyEmail";
 import './style/style.css'
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register/>} />
+          <Route path="/verifyemail" element={<VerifyEmail/>}/>
 
       </Routes>
     </BrowserRouter>
--- src/page/VerifyEmail/VerifyEmail.js
+++ src/page/VerifyEmail/VerifyEmail.js
@@ -1,20 +1,28 @@
 import { Button } from "@mui/material";
+import './VerifyEmail.css';
+
 
 export default function VerifyEmail ({user})
 {
+
     return (
         <div className="page">
-            <div className="conatiner">
+            <div className="container">
                 <h2 className="title">Verify Email</h2>
                 <div className="description">
                 <span> We've sent the 6-digit code to </span>
-                <span>{user.email}</span>
+                {/* <span>{user.email}</span> */}
                 <span>Enter the code below to confirm your email address.</span>
                 </div>
-                <div className="input">
+                <div className="inputWrapper">
+                    <input type="number" min="0" max="9" className="inputCode" ></input>
+                    <input type="number" min="0" max="9" className="inputCode" ></input>
+                    <input type="number" min="0" max="9" className="inputCode" ></input>
+                    <input type="number" min="0" max="9" className="inputCode" ></input>
+                    <input type="number" min="0" max="9" className="inputCode" ></input>
                     <input type="number" min="0" max="9" className="inputCode" ></input>
                 </div>
-                <Button type="submit" ></Button>
+                <Button type="submit" >Verify</Button>
             </div>
         </div>
     )
